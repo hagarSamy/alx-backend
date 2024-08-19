f2421bcbbbd0220818438efbcbbf255e30750b3a
hagarSamy <hs1800@fayoum.edu.eg>
2024-08-20 01:32:23 +0300

    hypermedia pagination

---
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+"""
+a function named index_range that takes
+two integer arguments page and page_size
+"""
+
+import csv
+import math
+from typing import List
+import logging
+
+
+def index_range(page: int, page_size: int) -> tuple:
+    '''a function to take page and page size
+    and returns a tuple of size two,
+    containing a 'start index' and an 'end index' corresponding to
+    the range of indexes
+    to return in a list for those particular pagination parameters.
+    '''
+
+    offset = (page - 1) * page_size
+    end = page * page_size
+    return (offset, end)
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        '''
+        Retrieve a specific page of data.
+        '''
+        assert type(page) is int and page > 0
+        assert type(page_size) is int and page_size > 0
+        data_set = self.dataset()
+        total_set_size = math.ceil(len(data_set) / page_size)
+
+        n_index_range = index_range(page=page, page_size=page_size)
+        if page > total_set_size:
+            return []
+        return data_set[n_index_range[0]: n_index_range[1]]
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
+        '''returns a dict of metadata'''
+        data = self.get_page(page=page, page_size=page_size)
+        data_len = len(data)
+        data_set = self.dataset()
+        total_set_size = math.ceil(len(data_set) / page_size)
+        if page == 1:
+            prev_page = None
+        else:
+            prev_page = page - 1
+        if page < total_set_size:
+            next_page = page + 1
+        else:
+            next_page = None
+        return {'page_size': page_size, 'page': page, 'data': data,
+                'next_page': next_page, 'prev_page': prev_page,
+                'total_pages': total_set_size}
+        
