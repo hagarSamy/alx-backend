43b428ca8ccc1ddb18eaa8f40a133dcdbbdd750a
hagarSamy <hs1800@fayoum.edu.eg>
2024-08-20 19:11:42 +0300

    Basic Caching

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""  a class BasicCache that
+inherits from BaseCaching and is a caching system
+"""
+
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    '''
+    A class BaseCaching -- a caching system
+    '''
+    def put(self, key, item):
+        '''setting a value to the key of the cache dict'''
+        self.cache_data[key] = item
+
+    def get(self, key):
+        '''
+        retrieving a value by the key
+        '''
+        try:
+            result = self.cache_data[key]
+        except KeyError:
+            return None
+        return result
