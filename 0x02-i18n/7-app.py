ccef0bc97042ff48fe1c1ecbda5be3b67837cf25
hagarSamy <hs1800@fayoum.edu.eg>
2024-08-28 02:12:28 +0300

    fixing the mismatch

--- 0x02-i18n/7-app.py
+++ 0x02-i18n/7-app.py
@@ -80,7 +80,7 @@ def get_timezone() -> str:
 @app.route('/')
 def index() -> str:
     """renders html page"""
-    return render_template('5-index.html')
+    return render_template('7-index.html')
 
 
 if __name__ == '__main__':
