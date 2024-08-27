c70e465fc7be01c63420ac21bc0ea07dbe854592
hagarSamy <hs1800@fayoum.edu.eg>
2024-08-27 17:41:03 +0300

    configuring the babel object and loading the configuration into the app

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -7,6 +7,16 @@ from flask_babel import Babel
 
 app = Flask(__name__)
 
+babel = Babel(app)
+
+class Config:
+    '''configure available languages in our app'''
+    LANGUAGES = ["en", "fr"]
+    # Configure the Babel object with the app
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+app.config.from_object(Config)
 
 @app.route('/')
 def index():
