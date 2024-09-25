401031666e82f9e3bc50202f488b76f0ec3c45d9
hagarSamy <hs1800@fayoum.edu.eg>
2024-09-25 04:41:43 +0300

    test

---
+++ 0x03-queuing_system_in_js/8-job.test.js
@@ -0,0 +1,39 @@
+import { expect } from 'chai';
+import kue from 'kue';
+import createPushNotificationsJobs from './8-job';
+
+describe('createPushNotificationsJobs', () => {
+  let queue;
+
+  before(() => {
+    queue = kue.createQueue();
+    queue.testMode.enter();
+  });
+
+  afterEach(() => {
+    queue.testMode.clear();
+  });
+
+  after(() => {
+    queue.testMode.exit();
+  });
+
+  it('should display an error message if jobs is not an array', () => {
+    expect(() => createPushNotificationsJobs('not an array', queue)).to.throw(Error, 'Jobs is not an array');
+  });
+
+  it('should create new jobs in the queue', () => {
+    const jobs = [
+      { phoneNumber: '5664911378', message: 'This is the code 9632 to verify your account' },
+      { phoneNumber: '2458678131', message: 'This is the code 5765 to verify your account' },
+    ];
+
+    createPushNotificationsJobs(jobs, queue);
+
+    expect(queue.testMode.jobs.length).to.equal(2);
+    expect(queue.testMode.jobs[0].type).to.equal('push_notification_code_3');
+    expect(queue.testMode.jobs[0].data).to.eql(jobs[0]);
+    expect(queue.testMode.jobs[1].type).to.equal('push_notification_code_3');
+    expect(queue.testMode.jobs[1].data).to.eql(jobs[1]);
+  });
+});
